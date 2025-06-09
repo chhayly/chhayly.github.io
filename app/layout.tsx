9207ceafc707f28aca8f21d20868fd2e275c021e
chhayly <chhayly.sreng@gmail.com>
2025-06-09 04:28:54 -0400

    Simplify design

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
       <head>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/css/style.css" />
       </head>
       <body>
